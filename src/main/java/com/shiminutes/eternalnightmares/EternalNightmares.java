445737c14f5f68cef556e2863f5dc50901cb8f72
3A <74498981+3agithub@users.noreply.github.com>
2025-03-31 22:05:29 +0800

    Initial commit

---
+++ src/main/java/com/shiminutes/eternalnightmares/EternalNightmares.java
@@ -0,0 +1,16 @@
+package com.shiminutes.eternalnightmares;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EternalNightmares implements ModInitializer {
+	public static final String MOD_ID = "eternal-nightmares";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/shiminutes/eternalnightmares/EternalNightmaresClient.java
@@ -0,0 +1,10 @@
+package com.shiminutes.eternalnightmares;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EternalNightmaresClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
